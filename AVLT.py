c0ca23288c7d9664cdf028929b25b782a33443eb
Eddie <eherrerag.eh@gmail.com>
2018-11-02 12:14:57 -0600

    Can return the number of anagrams of the word and the words that form the anagram. I am working in the function that is going to return the word with the longest anagram

--- AVLT.py
+++ AVLT.py
@@ -183,9 +183,10 @@ class AVLT:
         current_node = self.root
         while current_node is not None:
             # Compare the current node's key with the target key.
-            if current_node.key == key: return current_node
+            if current_node.key.lower() == key.lower(): return current_node
             elif current_node.key < key: current_node = current_node.right
             else: current_node = current_node.left
+        return None
 
     # Attempts to remove a node with a matching key. If no node has a matching key
     # then nothing is done and False is returned; otherwise the node is removed and
@@ -196,3 +197,75 @@ class AVLT:
             return False
         else:
             return self.remove_node(node)
+'''
+The Node class that the Tree is going to use.
+'''
+class Node:
+    # Constructor with a key parameter creates the Node object.
+    def __init__(self, key):
+        self.key = key
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.height = 0
+        
+    # Calculate the current nodes' balance factor,
+    # defined as height(left subtree) - height(right subtree)
+    def get_balance(self):
+        # Get current height of left subtree, or -1 if None
+        left_height = -1
+        if self.left is not None:
+            left_height = self.left.height
+            
+        # Get current height of right subtree, or -1 if None
+        right_height = -1
+        if self.right is not None:
+            right_height = self.right.height
+            
+        # Calculate the balance factor.
+        return left_height - right_height
+
+    # Recalculate the current height of the subtree rooted at
+    # the node, usually called after a subtree has been 
+    # modified.
+    def update_height(self):
+        # Get current height of left subtree, or -1 if None
+        left_height = -1
+        if self.left is not None:
+            left_height = self.left.height
+            
+        # Get current height of right subtree, or -1 if None
+        right_height = -1
+        if self.right is not None:
+            right_height = self.right.height
+
+        # Assign self.height with calculated node height.
+        self.height = max(left_height, right_height) + 1
+
+    # Assign either the left or right data member with a new child.
+    def set_child(self, which_child, child):
+
+        # Ensure which_child is properly assigned.
+        if which_child != "left" and which_child != "right":
+            return False
+
+        # Assign the left or right data member.
+        if which_child == "left":
+            self.left = child
+        else:
+            self.right = child
+        if child is not None:
+            child.parent = self
+        self.update_height()
+        return True
+
+    # Replace a current child with a new child.
+    def replace_child(self, current_child, new_child):
+        if self.left is current_child:
+            return self.set_child("left", new_child)
+        elif self.right is current_child:
+            return self.set_child("right", new_child)
+        
+        # If neither of the above cases applied, then the new child
+        # could not be attached to this node.
+        return False
--- Anagram.py
+++ Anagram.py
@@ -1,11 +1,51 @@
 from AVLT import AVLT
-from Node import Node
+from AVLT import Node
 
+#Going to read and insert the words into the tree
 def pop_tree(txt_file, tree):
     file = open(txt_file, "r")
     for line in file:
         l = line.split("\n")
         tree.insert(Node(l[0]))
 
+#Going to print the anagrams of the words and the count of the those anagram words
+def anagram(tree, word, prefix = "", p = True):
+        words = []
+        def print_anagram(word, prefix):
+                if len(word) <= 1:
+                        str = prefix + word
+                        n = tree.search(str)
+                        if n is not None:
+                                if p:
+                                        print(n.key)
+                                words.append(n.key)
+                else:
+                        for i in range(len(word)):
+                                cur = word [i: i + 1]
+                                before = word[0:i]
+                                after = word[i +1:]
+                                if cur not in before:
+                                        print_anagram(before + after, prefix + cur)
+        print_anagram(word, prefix)
+        if p:
+                print("Count: ", len(words))
+        return words
+
+#Going to find the greatest word with the most anagram in the file
+def greatest_anagram(file):
+        file = open(file, "r")
+        max = 0
+        word = ""
+        for line in file:
+                l = line.split("\n")
+                n = tree, l[0], "", False
+                if max < len(n):
+                        max = len(n)
+                        word = n
+        return word
+
+global tree
 tree = AVLT()
 pop_tree("words.txt", tree)
+anagram(tree, "money")
+print(greatest_anagram("words_example.txt"))
