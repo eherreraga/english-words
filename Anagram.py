2fa4785eec2cec9b1a741728e9d6f8986f5918f1
Eddie <eherrerag.eh@gmail.com>
2018-11-04 13:27:41 -0700

    The function for getting the word with the highest anagrams is working and averything with the AVL Tree is working. However, the RBT is not working for inserting the node

--- Anagram.py
+++ Anagram.py
@@ -1,12 +1,18 @@
 from AVLT import AVLT
 from AVLT import Node
+from RBT import RedBlackTree
+from RBT import RBTNode
 
 #Going to read and insert the words into the tree
 def pop_tree(txt_file, tree):
     file = open(txt_file, "r")
     for line in file:
         l = line.split("\n")
-        tree.insert(Node(l[0]))
+        #TODO red black and avl tree
+        if isinstance(tree, AVLT):
+                tree.insert(Node(l[0]))
+        else:
+                tree.insert(RBTNode(l[0]))
 
 #Going to print the anagrams of the words and the count of the those anagram words
 def anagram(tree, word, prefix = "", p = True):
@@ -38,14 +44,23 @@ def greatest_anagram(file):
         word = ""
         for line in file:
                 l = line.split("\n")
-                n = tree, l[0], "", False
+                n = anagram(tree, l[0], "", False)
                 if max < len(n):
                         max = len(n)
-                        word = n
-        return word
+                        word = n[0]
+        #Going to print the word with the most angrams along with the numeber of angrams
+        print(word,max)
 
 global tree
-tree = AVLT()
+# i = input("0)AVLT\n1)RBT\n")
+# if i is "0":
+#         print("Populating AVL tree...")
+#         tree = AVLT()
+# else:
+print("Populating RB tree...")
+tree = RedBlackTree()
 pop_tree("words.txt", tree)
 anagram(tree, "money")
-print(greatest_anagram("words_example.txt"))
+print("Finding the word with the greatest number of anagrams")
+greatest_anagram("words_example.txt")
+
